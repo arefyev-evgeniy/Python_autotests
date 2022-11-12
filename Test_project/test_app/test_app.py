8b61d8e2b494ab99aeb466ce94e19d9a8bba2622
Евгений Арефьев <111676852+arefyev-evgeniy@users.noreply.github.com>
2022-11-12 14:22:46 +0500

    Update test_app.py

--- Test_project/test_app/test_app.py
+++ Test_project/test_app/test_app.py
@@ -4,9 +4,9 @@ import requests
 import pytest
 
 def test_statuscode():
-    response = requests.get('https://petstore.swagger.io/v2/pet/531')
+    response = requests.get('https://petstore.swagger.io/v2/pet/12')
     assert response.status_code == 200
     
 def test_name():
-    response = requests.get('https://petstore.swagger.io/v2/pet/531')
+    response = requests.get('https://petstore.swagger.io/v2/pet/12')
     assert response.json()['status'] =='available'
